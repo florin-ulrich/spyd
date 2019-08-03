31e59383653862fc6920d2e2ca6f93f6b5ad0e9a
florin <e01606934@student.tuwien.ac.at>
2019-08-04 01:28:39 +0200

    devised gui that is more application-like

---
+++ src/main/java/spydGUI.java
@@ -0,0 +1,10 @@
+public class spydGUI {
+    // unified gui to handle the process of converting playlist to mp3s
+    // contains (subject to change):
+    //      input field for playlist id
+    //      button to convert playlist (greyed out during process)
+    //      resource container that models the state of the app
+    //      log text area logging every step
+    //      textfield that documents what is being done currently
+    //      pop ups to initiate next steps
+}
